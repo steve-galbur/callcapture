643b0fb92460dd6b54f2f8cf25f45303972e4afd
Steven Galbur <sgalbur@gmail.com>
2026-06-03 10:28:27 +0300

    fix

--- config/init.sql
+++ config/init.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE callcapture_production;
 CREATE DATABASE callcapture_queue;
-CREATE DATABASE callcapture_cable
-CREATE DATABASE callcapture_cache
+CREATE DATABASE callcapture_cable;
+CREATE DATABASE callcapture_cache;
+ALTER USER postgres WITH PASSWORD 'strong-password';
