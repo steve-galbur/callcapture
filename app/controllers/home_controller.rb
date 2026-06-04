1d0648ad318daf115910ccede464f9d7d186acff
Steven Galbur <sgalbur@gmail.com>
2026-06-04 13:02:52 +0300

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
+  def coming_soon
+    render layout: false
+  end
+
   def index
+    render layout: false
+  end
+
+  def admin
     render layout: false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get "home/index"
-  root "home#index"  
+  get "home/coming_soon"
+  get "dev"  => "home#index"
+  get "dev/admin" => "home#admin"
+  root "home#coming_soon"  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
